7caa8bbed913616f36369eced881076e7a3b4db3
kalakioski <71070273+kalakioski@users.noreply.github.com>
2021-11-19 13:37:36 +0200

    New screen MessagesScreen
    New screen: MessagesScreen
    New component: Screen, holds platform data for statusbar
    downloaded package: expo-constants

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
 
 const styles = StyleSheet.create({
