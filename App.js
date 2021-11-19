8bf053bc41f3e349f3b7bc536537563a5d9f2833
kalakioski <71070273+kalakioski@users.noreply.github.com>
2021-11-19 13:14:33 +0200

    New screen+ components + fixes
    New screen: ListingDetailsScreen
    Components: Card, ListItem
    Fixes: Deleted os AppTexts, Added colors, implemented icons to previous screens

--- App.js
+++ App.js
@@ -3,23 +3,11 @@ import { View, StyleSheet, StatusBar, Text } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="€100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
--- app/components/Card.js
+++ app/components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import { ReloadInstructions } from "react-native/Libraries/NewAppScreen";
 import colors from "../config/colors";
 import AppText from "./AppText";
@@ -8,8 +8,10 @@ function Card({ title, subTitle, image }) {
   return (
     <View style={styles.card}>
       <Image style={styles.image} source={image} />
-      <AppText>{title}</AppText>
-      <AppText>{subTitle}</AppText>
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
     </View>
   );
 }
@@ -19,10 +21,21 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: colors.white,
     marginBottom: 20,
+    overflow: "hidden",
+  },
+  detailsContainer: {
+    padding: 20,
   },
   image: {
     width: "100%",
     height: 200,
+  },
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+  title: {
+    marginBottom: 7,
   },
 });
 
